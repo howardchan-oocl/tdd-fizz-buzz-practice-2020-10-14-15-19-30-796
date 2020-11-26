943872c59136960fc8a2fed099519804addc7d53
chanho <howard.chan@oocl.com>
2020-11-26 14:28:05 +0800

    [FizzBuzz][Howard Chan]fix typo

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,27 +2,27 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
-    public static final String FIZZ = "Fizz";
-    public static final int MODULE_3 = 3;
-    public static final String BUZZ = "Buzz";
-    public static final int MODULE_5 = 5;
-    public static final String WHIZZ = "Whizz";
-    public static final int MODULE_7 = 7;
+    private static final String FIZZ = "Fizz";
+    private static final int MODULO_3 = 3;
+    private static final String BUZZ = "Buzz";
+    private static final int MODULO_5 = 5;
+    private static final String WHIZZ = "Whizz";
+    private static final int MODULO_7 = 7;
 
     public String countOff(int order) {
-        if (order % MODULE_3 == 0 && order % MODULE_5 == 0 && order % MODULE_7 == 0) {
+        if (order % MODULO_3 == 0 && order % MODULO_5 == 0 && order % MODULO_7 == 0) {
             return FIZZ + BUZZ + WHIZZ;
-        } else if (order % MODULE_3 == 0 && order % MODULE_5 == 0) {
+        } else if (order % MODULO_3 == 0 && order % MODULO_5 == 0) {
             return FIZZ + BUZZ;
-        } else if (order % MODULE_3 == 0 && order % MODULE_7 == 0) {
+        } else if (order % MODULO_3 == 0 && order % MODULO_7 == 0) {
             return FIZZ + WHIZZ;
-        } else if (order % MODULE_5 == 0 && order % MODULE_7 == 0) {
+        } else if (order % MODULO_5 == 0 && order % MODULO_7 == 0) {
             return BUZZ + WHIZZ;
-        } else if (order % MODULE_3 == 0) {
+        } else if (order % MODULO_3 == 0) {
             return FIZZ;
-        } else if (order % MODULE_5 == 0) {
+        } else if (order % MODULO_5 == 0) {
             return BUZZ;
-        } else if (order % MODULE_7 == 0) {
+        } else if (order % MODULO_7 == 0) {
             return WHIZZ;
         }
         return String.valueOf(order);
