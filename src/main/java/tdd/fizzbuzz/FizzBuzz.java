8ef622e43f91e7a1c01210b2e54838e368ace9e6
chanho <howard.chan@oocl.com>
2020-11-26 14:09:10 +0800

    [FizzBuzz][Howard Chan]implemented the requirement of returning Buzz when input is multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,10 +4,14 @@ public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final int MODULE_3 = 3;
+    public static final String BUZZ = "Buzz";
+    public static final int MODULE_5 = 5;
 
     public String countOff(int order) {
         if (order % MODULE_3 == 0) {
             return FIZZ;
+        }else if (order % MODULE_5 == 0) {
+            return BUZZ;
         }
         return String.valueOf(order);
     }
