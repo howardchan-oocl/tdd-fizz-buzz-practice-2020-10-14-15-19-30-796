d97183ffc277f2a2cba4106c3c46bde4464f5f32
chanho <howard.chan@oocl.com>
2020-11-26 14:11:41 +0800

    [FizzBuzz][Howard Chan]implemented the requirement of return Whizz when input is multiple of 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -6,12 +6,16 @@ public class FizzBuzz {
     public static final int MODULE_3 = 3;
     public static final String BUZZ = "Buzz";
     public static final int MODULE_5 = 5;
+    public static final String WHIZZ = "Whizz";
+    public static final int MODULE_7 = 7;
 
     public String countOff(int order) {
         if (order % MODULE_3 == 0) {
             return FIZZ;
         }else if (order % MODULE_5 == 0) {
             return BUZZ;
+        }else if (order % MODULE_7 ==0) {
+            return WHIZZ;
         }
         return String.valueOf(order);
     }
