270fbd249bea276a44dc2b68dea75ecbe01cf257
chanho <howard.chan@oocl.com>
2020-11-26 13:57:03 +0800

    [FizzBuzz][Howard Chan]implement the test of normal order number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_order_number_when_input_normal_order_number() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        int order = 1;
+
+        assertEquals("1", fizzBuzz.countOff(order));
+    }
 }
