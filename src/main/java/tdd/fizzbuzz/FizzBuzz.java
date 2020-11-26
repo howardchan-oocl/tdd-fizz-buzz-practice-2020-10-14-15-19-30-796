beb86017490d933c86d9fa33a55e30db7e0c4e4d
chanho <howard.chan@oocl.com>
2020-11-26 14:05:51 +0800

    [FizzBuzz][Howard Chan]implemented the requirement of returning Fizz when input is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,14 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+    public static final int MODULE_3 = 3;
+
     public String countOff(int order) {
+        if (order % MODULE_3 == 0) {
+            return FIZZ;
+        }
         return String.valueOf(order);
     }
 }
